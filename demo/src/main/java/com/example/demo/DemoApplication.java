25446819a66a93609aa4b0bfe5f2b6b43d7b2c39
Sanele Vilakazi <sanele@wethinkcode.co.za>
2025-03-10 00:20:30 +0200

    "Entity Learning "

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.model.Student;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> listofStudent(){
+		return List.of(new Student(
+									1L,
+									"John",
+									"johndoe@email.com",
+									LocalDate.of(2000,02,14),
+									"25"));
 	}
 
 }
